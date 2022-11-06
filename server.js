1420ed675390f5b8165c9746a2539d9ed61a73b0
Hyeonmin Seon <hmsun.es@esgroup.net>
2022-11-06 22:17:04 +0900

    깔끔하게 정리

--- server.js
+++ server.js
@@ -5,16 +5,41 @@ const PORT = process.env.PORT || 8000;
 
 const eventHandler = require('./eventHandler');
 
-const server = http.createServer((req, res) => {
+const serveFile = async (filePath, contentType, statusCode, response) => {
+    try {
+        const data = await fs.promises.readFile(filePath);
+        response.writeHead(statusCode, {
+            'Content-Type': contentType,
+        });
+        response.end(data);
+    } catch (error) {
+        eventHandler.emit('log', `${error.name}: ${error.message}`, 'errLog.txt');
+    }
+};
+
+const server = http.createServer((request, response) => {
     // console.log(`---------------${req.url}----------------------`);
     // console.log(req.rawHeaders, req.headers, req.method, req.url);
     // console.log('----------------------------------------');
-    eventHandler.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+    eventHandler.emit('log', `${request.url}\t${request.method}`, 'reqLog.txt');
 
-    const extension = path.extname(req.url);
+    const extension = path.extname(request.url);
+
+    const useViewFile = !extension || extension === '.html';
+    let filePath = path.join(__dirname, useViewFile ? 'views' : '', `${request.url}`);
+    if (!extension && fs.existsSync(path.join(filePath, 'index.html'))) {
+        filePath = path.join(filePath, 'index.html');
+    } else if (!extension && fs.existsSync(filePath + '.html')) {
+        filePath += '.html';
+    }
+    let statusCode = 200;
+
+    if (!fs.existsSync(filePath)) {
+        statusCode = 404;
+        filePath = path.join(__dirname, 'views/404.html');
+    }
 
     let contentType = '';
-
     switch (extension) {
         case '.jpg':
             contentType = 'image/png';
@@ -33,14 +58,7 @@ const server = http.createServer((req, res) => {
             break;
     }
 
-    try {
-        fs.readFile(path.join(__dirname, req.url), '', (err, data) => {
-            res.writeHead(200, { 'Content-Type': contentType });
-            res.end(data);
-        });
-    } catch (error) {}
+    serveFile(filePath, contentType, statusCode, response);
 });
 
-server.listen(PORT, () =>
-    console.log(`Server running on http://localhost:${PORT}`)
-);
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
